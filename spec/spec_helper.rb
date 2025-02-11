3b05a4ceb4fd8c3cc228bd9fa10597dc1d504d1e
thadeu <tadeuu@gmail.com>
2025-02-11 00:10:26 -0300

    chore: spec_helper with new config to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,12 +7,8 @@ Dir["#{File.expand_path(__dir__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
-  # config.example_status_persistence_file_path = '.rspec_status'
-  # config.filter_run :focus
+  config.example_status_persistence_file_path = '.rspec_status'
+  config.filter_run :focus
 
   config.order = :random
-
-  # config.expect_with :rspec do |c|
-  #   c.syntax = :expect
-  # end
 end
